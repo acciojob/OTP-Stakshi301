4085abe62d8e522e28544158240452910a12a672
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-10-29 16:14:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var inputs = document.querySelectorAll('.code');
+inputs.forEach((input, index) => {
+    input.addEventListener('keyup', (e) => {
+        if(e.key >= 0 && e.key <= 9) {
+            if(index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        }
+    });
+});
