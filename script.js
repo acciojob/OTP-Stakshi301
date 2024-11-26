86e09df85a70b9b91b5f37fb86c1ec895d74e73b
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-11-26 14:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// Inside script.js
-const inputs = document.querySelectorAll('.code');  // Make sure this line is present
+const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
-    input.addEventListener('input', (e) => {
-        if (e.target.value.length > 0 && index < inputs.length - 1) {
-            inputs[index + 1].focus();
-        }
-    });
+  input.addEventListener('input', (e) => {
+    // Move to the next input when a digit is entered
+    if (e.target.value.length > 0 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+      console.log(`Focused: ${inputs[index + 1].id}`);
+    }
+  });
 
-    input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
-            inputs[index - 1].focus();
-        }
-    });
+  input.addEventListener('keydown', (e) => {
+    // Move to the previous input when backspace is pressed
+    if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
+      inputs[index - 1].focus();
+      console.log(`Focused: ${inputs[index - 1].id}`);
+    }
+  });
 });
