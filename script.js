b0286fa4cc181b48938dcfd826e16937b57b38d7
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-11-28 15:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const inputs = document.querySelectorAll('.code');
 
   inputs.forEach((input, index) => {
-    input.addEventListener('input', (e) => {
-      const value = e.target.value;
-      if (value && index < inputs.length - 1) {
+    input.addEventListener('input', () => {
+      if (input.value && index < inputs.length - 1) {
         inputs[index + 1].focus();
       }
     });
 
     input.addEventListener('keydown', (e) => {
-      if (e.key === 'Backspace') {
-        if (!e.target.value && index > 0) {
-          inputs[index - 1].focus();
-        }
+      if (e.key === 'Backspace' && !input.value && index > 0) {
+        inputs[index - 1].focus();
       }
     });
   });
